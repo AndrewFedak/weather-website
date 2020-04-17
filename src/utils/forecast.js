f9bdc08e3125bac460c32fe7abbd6f09413ab38f
riba12331 <fedakandrew@gmail.com>
2020-04-17 22:11:12 +0300

    Changed to degree Celsium

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require("request");
 
 const forecast = (latitude, longitude, callback) => {
-    const url = `http://api.weatherstack.com/current?access_key=aaee407b4ae8b2280930c472aea11696&query=${latitude},${longitude}&units=f`;
+    const url = `http://api.weatherstack.com/current?access_key=aaee407b4ae8b2280930c472aea11696&query=${latitude},${longitude}&units=d`;
 
         request({ url, json: true }, (error, response) => {
             const {error: bodyError, current} = response.body;
